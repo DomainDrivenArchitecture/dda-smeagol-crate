b8283ca407aae79057ea956751a6c6006332ce5e
jem <michael.jerger@meissa-gmbh.de>
2018-12-28 21:00:50 +0100

    deploy only taged builds

--- integration/docker/publish.sh
+++ integration/docker/publish.sh
@@ -2,6 +2,6 @@
 
 docker_repo=$1
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag dda-smeagol-crate domaindrivenarchitecture/$docker_repo:latest
-docker push $DOCKER_USERNAME/$docker_repo
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker tag ${docker_repo} ${DOCKER_USERNAME}/${docker_repo}:latest
+docker push ${DOCKER_USERNAME}/${docker_repo}
