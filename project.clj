a65f00d0b0ad7269a622a716887d0b7da05a63ab
jem <michael.jerger@meissa-gmbh.de>
2018-12-29 11:04:47 +0100

    upd. deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[dda/dda-pallet "2.2.2"]
-                 [dda/dda-git-crate "2.1.4"]
+                 [dda/dda-git-crate "2.2.0"]
                  [dda/dda-httpd-crate "2.1.0"]
                  [dda/dda-user-crate "1.0.6"]
                  [clj-http "3.9.1"]
